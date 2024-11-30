2aca9c2955d3918c88eeee9379f2e270e2687b2b
Volodymyr Zakhovaiko <zaxovaiko@gmail.com>
2024-11-30 14:46:09 +0100

    Added payload cms config and init files and packages

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+import { withPayload } from "@payloadcms/next/withPayload";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+};
+
+export default withPayload(nextConfig);
