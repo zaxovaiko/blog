6c88ec00bb6d8387da7eb185ba85ceb0356c0554
Volodymyr Zakhovaiko <zaxovaiko@gmail.com>
2025-05-12 00:54:20 +0200

    feat: add syntax hightlight

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,7 +1,31 @@
 import type { MDXComponents } from "mdx/types";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { darcula } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
+    code: (props) => {
+      const { className, children } = props;
+      const language = className?.replace(/language-/, "") || "";
+      if (
+        !className ||
+        SyntaxHighlighter.supportedLanguages.indexOf(language) === -1
+      ) {
+        return <code {...props} />;
+      }
+      return (
+        <SyntaxHighlighter
+          showLineNumbers
+          language={language}
+          customStyle={{
+            backgroundColor: "transparent",
+          }}
+          style={darcula}
+        >
+          {children}
+        </SyntaxHighlighter>
+      );
+    },
   };
 }
