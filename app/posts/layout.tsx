85141a4188efb9aba1ef6943164389cc5fbbd6e1
Volodymyr Zakhovaiko <zaxovaiko@gmail.com>
2025-05-18 19:02:52 +0200

    fix: change text color

--- app/posts/layout.tsx
+++ app/posts/layout.tsx
@@ -9,7 +9,7 @@ export default function MdxLayout({ children }: { children: React.ReactNode }) {
           <span className="text-sm">Back to all posts</span>
         </Button>
       </Link>
-      <article className="flex flex-col prose dark:prose-invert prose-invert py-10 prose-h1:mb-0 prose-h1:mt-10 prose-h2:mt-3 prose-h2:mb-3 prose-blockquote:border-foreground/20 prose-blockquote:text-foreground/50 prose-a:text-accent prose-hr:mt-4 prose-hr:mb-0">
+      <article className="text-stone-400 flex flex-col prose dark:prose-invert prose-invert py-10 prose-h1:mb-0 prose-h1:mt-10 prose-h2:mt-3 prose-h2:mb-3 prose-blockquote:border-foreground/20 prose-blockquote:text-foreground/50 prose-a:text-accent prose-hr:mt-4 prose-hr:mb-0">
         {children}
       </article>
     </div>
